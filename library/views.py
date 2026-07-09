01248ac9669aacd30dc5eed8922ea135d34fabf7
ArsisNoori <arsisnoori0@gmail.com>
2026-07-09 23:15:44 +0330

    Add DjangoFilterBackend

--- library/views.py
+++ library/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,6 +12,36 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
-    search_fields = ('title', 'author', 'isbn')
-    ordering_fields =  ("price", "published_year", "pages")
+    filter_backends = (
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    )
+
+    class BookViewSet(viewsets.ModelViewSet):
+        queryset = Book.objects.all()
+        serializer_class = BookSerializer
+
+        filter_backends = (
+            filters.SearchFilter,
+            filters.OrderingFilter,
+            DjangoFilterBackend,
+        )
+
+        search_fields = (
+            "title",
+            "author",
+            "isbn",
+        )
+
+        ordering_fields = (
+            "price",
+            "published_year",
+            "pages",
+        )
+
+        filterset_fields = (
+            "is_available",
+            "published_year",
+        )
+
